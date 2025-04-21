d11a446eae9416103992729b3e3978f9dc555dd3
Clayton Knittel <cknit1999@gmail.com>
2025-04-21 15:34:56 -0700

    Page selection

--- src/app.rs
+++ src/app.rs
@@ -1,14 +1,34 @@
-use yew::{classes, function_component, html, Html};
+use yew::{classes, function_component, html, use_state_eq, Callback, Html, Properties};
+
+#[derive(Clone, PartialEq, Eq)]
+enum Page {
+  Home,
+  Games,
+  Projects,
+}
+
+#[derive(Properties, PartialEq)]
+struct Props {
+  page: Page,
+  change_page: Callback<Page>,
+}
 
 #[function_component(Header)]
-fn header() -> Html {
+fn header(props: &Props) -> Html {
+  let change_page = props.change_page.clone();
+  let to_home_page = move |_| change_page.clone().emit(Page::Home);
+  let change_page = props.change_page.clone();
+  let to_games_page = move |_| change_page.clone().emit(Page::Games);
+  let change_page = props.change_page.clone();
+  let to_projects_page = move |_| change_page.clone().emit(Page::Projects);
+
   html! {
     <div class={classes!("header")}>
       <div class={classes!("title")}>{ "cknittel.com" }</div>
       <div class={classes!("menu")}>
-        <div class={classes!("menuitem")}>{"Home"}</div>
-        <div class={classes!("menuitem")}>{"Games"}</div>
-        <div class={classes!("menuitem")}>{"Projects"}</div>
+        <div class={classes!("menuitem")} onclick={to_home_page}>{"Home"}</div>
+        <div class={classes!("menuitem")} onclick={to_games_page}>{"Games"}</div>
+        <div class={classes!("menuitem")} onclick={to_projects_page}>{"Projects"}</div>
       </div>
     </div>
   }
@@ -23,9 +43,11 @@ fn body() -> Html {
 
 #[function_component(App)]
 pub fn app() -> Html {
+  let page = use_state_eq(|| Page::Home);
+
   html! {
     <>
-      <Header />
+      <Header page={(*page).clone()} change_page={move |page_id| page.set(page_id)} />
       <Body />
     </>
   }
