86c7ffd01aa7d4cb6459fefbb51ee5b8afe38c89
Clayton Knittel <cknit1999@gmail.com>
2025-04-21 16:42:54 -0700

    Auto generate menu

--- src/header.rs
+++ src/header.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+use strum::{IntoEnumIterator, VariantMetadata};
 use yew::{classes, function_component, html, Callback, Html, Properties};
 
 use crate::page::Page;
@@ -10,20 +12,23 @@ pub struct Props {
 
 #[function_component(Header)]
 pub fn header(props: &Props) -> Html {
-  let change_page = props.change_page.clone();
-  let to_home_page = move |_| change_page.clone().emit(Page::Home);
-  let change_page = props.change_page.clone();
-  let to_games_page = move |_| change_page.clone().emit(Page::Games);
-  let change_page = props.change_page.clone();
-  let to_projects_page = move |_| change_page.clone().emit(Page::Projects);
+  let pages = Page::iter()
+    .map(|page| {
+      let change_page = props.change_page.clone();
+      html! {
+        <div
+          class={classes!("menuitem")}
+          onclick={move |_| change_page.emit(page)}
+        >{page.variant_name()}</div>
+      }
+    })
+    .collect_vec();
 
   html! {
     <div class={classes!("header")}>
       <div class={classes!("title")}>{ "cknittel.com" }</div>
       <div class={classes!("menu")}>
-        <div class={classes!("menuitem")} onclick={to_home_page}>{"Home"}</div>
-        <div class={classes!("menuitem")} onclick={to_games_page}>{"Games"}</div>
-        <div class={classes!("menuitem")} onclick={to_projects_page}>{"Projects"}</div>
+        {pages}
       </div>
     </div>
   }
--- src/page.rs
+++ src/page.rs
@@ -1,6 +1,17 @@
-#[derive(Clone, Copy, PartialEq, Eq)]
+use strum::{EnumCount, EnumIter, VariantMetadata, VariantNames};
+
+#[derive(Clone, Copy, PartialEq, Eq, EnumCount, EnumIter, VariantNames)]
 pub enum Page {
   Home,
   Games,
   Projects,
+}
+
+impl VariantMetadata for Page {
+  const VARIANT_COUNT: usize = Self::COUNT;
+  const VARIANT_NAMES: &'static [&'static str] = Self::VARIANTS;
+
+  fn variant_name(&self) -> &'static str {
+    Self::VARIANTS[*self as usize]
+  }
 }
